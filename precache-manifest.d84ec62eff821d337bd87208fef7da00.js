242451c38cd6d5bbedfc29b7f78b7c6e5c82a90d
yossi <joe.eshel@gmail.com>
2020-10-23 11:57:32 +0300

    first commit

---
+++ precache-manifest.d84ec62eff821d337bd87208fef7da00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1abe3ff39af510e744a4bb8fa1f5183",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c703d21499074d78241",
+    "url": "/static/css/main.81fb8516.chunk.css"
+  },
+  {
+    "revision": "c7604da839d38b4d9f69",
+    "url": "/static/js/2.899e873d.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.899e873d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c703d21499074d78241",
+    "url": "/static/js/main.65dcccd5.chunk.js"
+  },
+  {
+    "revision": "5fb11a1c23db92eab440",
+    "url": "/static/js/runtime-main.ebed117f.js"
+  }
+]);
